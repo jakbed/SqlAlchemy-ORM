6e8f52b6b0dc46fb2618aabad738cd58f34bb82f
Jakub <jakub.bednarczyk@gmail.com>
2023-05-14 12:38:15 +0200

    Adding salary and creation date Columns

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Integer, Column, String, create_engine
+from sqlalchemy import Integer, Column, String, create_engine, DateTime, Float
 from sqlalchemy.ext import declarative
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from datetime import datetime
 
 engine = create_engine(
     "mysql+pymysql://root:password@localhost:3306/company"
@@ -17,6 +18,12 @@ class User(Base):
     first_name = Column(String(50), nullable=False)
     last_name = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False, unique=True)
+    salary = Column(Float, default=0, nullable=False)
+    creation_date = Column(
+        DateTime,
+        default=datetime.now(),
+        nullable=False
+    )
 
     def __repr__(self):
         return f"User {self.first_name}  {self.last_name}>"
